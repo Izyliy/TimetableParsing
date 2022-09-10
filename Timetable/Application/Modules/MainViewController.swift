10ced1a4a20614b6249a16f774d83cb23e982858
Pavel Grabchak <pavelgrabchak@icloud.com>
2022-09-10 22:18:03 +0300

    feat: edited temporary settings VC

--- Timetable/Application/Modules/MainViewController.swift
+++ Timetable/Application/Modules/MainViewController.swift
@@ -12,7 +12,8 @@ class MainViewController: UIViewController {
     let label: UILabel = {
         let label = UILabel()
         
-        label.text = "fcwefdcwe"
+        label.text = "  Настройки  "
+        label.font = .systemFont(ofSize: 26)
         label.backgroundColor = .brown
         label.textColor = .white
         
@@ -21,7 +22,16 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 1)
+        view.backgroundColor = .white //UIColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 1)
+        
+        view.addSubview(label)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 
 }
