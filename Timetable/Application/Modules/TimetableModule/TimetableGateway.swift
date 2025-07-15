21e1f4009c7eb83091aceff3a453654ee4b90fc3
Pavel Grabchak <pavelgrabchak@icloud.com>
2025-07-15 15:01:51 +0300

    feat: added test timetable

--- Timetable/Application/Modules/TimetableModule/TimetableGateway.swift
+++ Timetable/Application/Modules/TimetableModule/TimetableGateway.swift
@@ -13,6 +13,8 @@ class TimetableGateway {
     
     //https://s.kubsau.ru/?type_schedule=1&val=ПИ2241
     func getTimetableHtmlFor(name: String, type: TimetableType) -> Promise<String> {
+        guard name != "t1ES2202" else { return getTestTimetable() }
+        
         let promise = Promise<String>.pending()
         let url = URL(string: "https://s.kubsau.ru/")!
         
@@ -23,6 +25,28 @@ class TimetableGateway {
                 .init(name: "type_schedule", value: type.getTypeNumber()),
                 .init(name: "val", value: name)
             ])
+            .makeRequest(withBaseURL: url)
+            
+        RequestManager.sharedInstance.makeDataRequest(urlRequest).then { data in
+            if let contents = String(data: data, encoding: .utf8) {
+                promise.fulfill(contents)
+            } else {
+                promise.reject(NSError(domain: "Can't get string out of data", code: 0))
+            }
+        }.catch { error in
+            promise.reject(error)
+        }
+        
+        return promise
+    }
+    
+    func getTestTimetable() -> Promise<String> {
+        let promise = Promise<String>.pending()
+        let url = URL(string: "https://web.archive.org/web/20250302065604/https://s.kubsau.ru/?type_schedule=1&val=ЭС2202")!
+        
+        let urlRequest = URLRequestBuilder(path: "")
+            .method(.get)
+            .timeout(60)
             .makeRequest(withBaseURL: url)
             
         RequestManager.sharedInstance.makeDataRequest(urlRequest).then { data in
